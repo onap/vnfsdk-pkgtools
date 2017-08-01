26313139a163c9b92793a022d025ce64029db68f
Lianhao Lu <lianhao.lu@intel.com>
2017-08-01 15:01:24 +0800

    Unified dependency requirement into single file
    Use the requirements.txt as the single file to include all the
    dependencies.
    
    Change-Id: If68b61826bfe8d32d08e59d9b67a8791cc878833
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@
 # under the License.
 #
 
+import os
 from setuptools import setup
 import sys
 
@@ -23,6 +24,28 @@ if sys.version_info < (2, 7):
     sys.exit('VNF SDK requires Python 2.7+')
 if sys.version_info >= (3, 0):
     sys.exit('VNF SDK does not support Python 3')
+
+
+root_dir = os.path.dirname(__file__)
+install_requires = []
+extras_require = {}
+
+with open(os.path.join(root_dir, 'requirements.txt')) as requirements:
+    for requirement in requirements.readlines():
+        # get rid of comments or trailing comments
+        requirement = requirement.split('#')[0].strip()
+        if not requirement:
+            continue # skip empty and comment lines
+        # dependencies which use environment markers have to go in as
+        # conditional dependencies under "extra_require", see more at:
+        # https://wheel.readthedocs.io/en/latest/index.html#defining-conditional-dependencies
+        if ';' in requirement:
+            package, condition = requirement.split(';')
+            cond_name = ':{0}'.format(condition.strip())
+            extras_require.setdefault(cond_name, [])
+            extras_require[cond_name].append(package.strip())
+        else:
+            install_requires.append(requirement)
 
 setup(
     name='vnfsdk',
@@ -62,9 +85,8 @@ setup(
         'console_scripts': [
             'vnfsdk = cli.__main__:main']
     },
-    # Please make sure this is in sync with src/aria/requirements.txt
-    install_requires=[
-        'ruamel.yaml<0.12.0,>=0.11.12',
-        'requests<2.14.0,>=2.3.0',
-        'apache-ariatosca==0.1.1'])
+
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require=extras_require)
 
